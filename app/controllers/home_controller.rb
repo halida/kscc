4cc1f1c4db393f034e0f473f770feeef3e6167d6
halida <linjunhalida@gmail.com>
2012-11-23 22:22:32 +0800

    add frontpage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,5 @@
 class HomeController < ApplicationController
+  def frontpage
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Kscc::Application.routes.draw do
+
+  root to: 'home#frontpage'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
