6a12fa49c75e0734e632877cd01e4fe0eb404d64
halida <linjunhalida@gmail.com>
2012-11-23 21:58:18 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kscc::Application.config.secret_token = '09bb0037e956364eb1195643560dfcc95647e8d5409d50c35534c0f7aef7cf4d8af31db05f779dc61b2c3fa422ad5fca76c2aaa7be8f3bfd46034f3653895346'
